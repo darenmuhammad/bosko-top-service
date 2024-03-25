d79046c147b2fdd2938e77bbec0b84f995dd8862
Daren Muhammad <darenmuhammad10@gmail.com>
2024-03-25 07:39:56 +0700

    majority cahanges

--- detail.js
+++ detail.js
@@ -2,20 +2,20 @@
 const detailBtn = document.querySelector('.detail-btn');
 const detailOrang = document.querySelector('.detail-outline');
 const exitBtn = document.querySelector('.exit-btn');
-const team = document.querySelector('#team');
+const group = document.querySelector('#group');
 const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
 
 detailBtn.onclick = () => {
     detailOrang.classList.add('active');
-    team.classList.add('active');
+    group.classList.add('active');
     header.classList.add('active');
     footer.classList.add('active');
 };
 
 exitBtn.onclick = () => {
     detailOrang.classList.remove('active');
-    team.classList.remove('active');
+    group.classList.remove('active');
     header.classList.remove('active');
     footer.classList.remove('active');
 };
